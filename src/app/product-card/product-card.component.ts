913e31e6fe22e1aff87b104a4bb33dfaf78230f5
arjun-saseendran <arjunsaseendran@icloud.com>
2024-10-31 13:31:26 +0530

    finish product-card

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface Product {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  image: string;
+}
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-card.component.html',
-  styleUrl: './product-card.component.css'
+  styleUrl: './product-card.component.css',
 })
 export class ProductCardComponent {
+  products: Product[] = [];
 
+  ngOnInit() {
+    this.fetchProducts();
+  }
+
+  fetchProducts() {
+    fetch('https://fakestoreapi.com/products/')
+      .then((res) => res.json())
+      .then((data: Product[]) => {
+        this.products = data;
+      });
+  }
 }
