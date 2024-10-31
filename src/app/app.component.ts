5a6d10b84c050293e7e207a06f9d3afa60a25d31
arjun-saseendran <arjunsaseendran@icloud.com>
2024-10-31 13:37:07 +0530

    finish footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { ProductCardComponent } from './product-card/product-card.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ProductCardComponent],
+  imports: [RouterOutlet, HeaderComponent, ProductCardComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
